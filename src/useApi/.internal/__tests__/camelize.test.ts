e2425eabecbd485dac4ec21fb9764d5a4dc8f902
myeongju Mun <mym0404@gmail.com>
2020-03-12 01:25:54 +0900

    sample test

--- src/useApi/.internal/__tests__/camelize.test.ts
+++ src/useApi/.internal/__tests__/camelize.test.ts
@@ -3,5 +3,14 @@ import camelize from "../camelize";
 describe('convert strings to camelCase!', () => {
   it('white space strings to camelCase', () => {
     expect(camelize('')).toBe('')
+    expect(camelize('doo boo')).toBe('dooBoo')
+    expect(camelize('')).toBe('')
+    expect(camelize('')).toBe('')
+    expect(camelize('')).toBe('')
+    expect(camelize('')).toBe('')
+    expect(camelize('')).toBe('')
+    expect(camelize('')).toBe('')
+    expect(camelize('')).toBe('')
+    expect(camelize('')).toBe('')
   });
 });
