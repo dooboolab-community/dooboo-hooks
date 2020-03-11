859f15fbfb88d2f63489662944b515d7fd0c7202
myeongju Mun <mym0404@gmail.com>
2020-03-11 22:59:43 +0900

    Add Dependency 'jest'
    add jest to dependency and initial setup

---
+++ jest.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  preset: 'react-native',
+  automock: false,
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '\\.(ts|tsx)$': 'ts-jest',
+  },
+  modulePaths: ['<rootDir>'],
+  moduleDirectories: ['node_modules'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'svg', 'png', 'json'],
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        jsx: 'react',
+      },
+      diagnostics: false,
+      babelConfig: true,
+    },
+  },
+  modulePathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/', '<rootDir>/.history/'],
+  // 'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
+  cacheDirectory: '.jest/cache',
+  // setupFilesAfterEnv: ['./test/setupTest.ts'],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'babel-jest',
+  },
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+    '<rootDir>/test/jestSetup.ts',
+  ],
+  // 'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  /* eslint-disable */
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@dooboo-ui)',
+  ],
+  /* eslint-enable */
+  haste: {
+    defaultPlatform: 'ios',
+    platforms: ['android', 'ios', 'native'],
+    providesModuleNodeModules: ['react', 'react-native'],
+  },
+  coveragePathIgnorePatterns: ['/node_modules/'],
+};
---
+++ test/jestSetup.ts
@@ -0,0 +1,5 @@
+/**
+ * monkey patching the locale to avoid the error:
+ * Something went wrong initializing the native ReactLocalization module
+ * https://gist.github.com/MoOx/08b465c3eac9e36e683929532472d1e0
+ */
