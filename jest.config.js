6a05887d4dd617c8accf400d4c0f9aaf7d73e693
myeongju Mun <mym0404@gmail.com>
2020-03-12 01:09:44 +0900

    continue jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,44 +1,5 @@
 module.exports = {
-  automock: false,
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-    '\\.(ts|tsx)$': 'ts-jest',
-  },
-  modulePaths: ['<rootDir>'],
-  moduleDirectories: ['node_modules'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  moduleFileExtensions: ['js', 'ts', 'tsx', 'svg', 'png', 'json'],
-  globals: {
-    'ts-jest': {
-      tsConfig: {
-        jsx: 'react',
-      },
-      diagnostics: false,
-      babelConfig: true,
-    },
-  },
-  modulePathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/', '<rootDir>/.history/'],
-  // 'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
-  cacheDirectory: '.jest/cache',
-  // setupFilesAfterEnv: ['./test/setupTest.ts'],
-  moduleNameMapper: {
-    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'babel-jest',
-  },
-  setupFiles: [
-    '<rootDir>/test/jestSetup.ts',
-  ],
-  // 'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  /* eslint-disable */
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@dooboo-ui)',
-  ],
-  /* eslint-enable */
-  haste: {
-    defaultPlatform: 'ios',
-    platforms: ['android', 'ios', 'native'],
-    providesModuleNodeModules: ['react', 'react-native'],
-  },
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['node_modules', 'lib'],
 };
