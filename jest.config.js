4374556437d8222077ef0555a6b07b9685590342
myeongju Mun <mym0404@gmail.com>
2020-03-13 02:24:25 +0900

    Add `fetchMock`

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['node_modules', 'dist'],
+  setupFiles: ['<rootDir>/test/jestSetup.ts'],
 };
---
+++ test/jestSetup.ts
@@ -0,0 +1,7 @@
+import { GlobalWithFetchMock } from 'jest-fetch-mock';
+
+declare const global;
+
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;
