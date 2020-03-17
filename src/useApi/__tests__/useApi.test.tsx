fffaa8fe30f373813d25495f86d4ba42e63bbc76
myeongju Mun <mym0404@gmail.com>
2020-03-17 11:06:55 +0900

    Basic Testing

--- src/useApi/__tests__/useApi.test.tsx
+++ src/useApi/__tests__/useApi.test.tsx
@@ -1,16 +1,50 @@
 import { ApiResult } from '../internal/ApiClient';
+import { renderHook } from '@testing-library/react-hooks';
+import useApi from '../useApi';
 
-let mockApi = jest.fn();
+type ResponseType = {
+  name: string;
+  age: number;
+};
+const responseData: ResponseType = { name: 'dooboo', age: 24 };
 let mockUnsubscribe = jest.fn();
 let mockCall = jest.fn();
+let apiResult: ApiResult<ResponseType>;
 
-describe('useApi Test', () => {
+describe('useApi', () => {
   beforeEach(() => {
+    apiResult = [mockCall, mockUnsubscribe];
     mockUnsubscribe.mockReset();
     mockCall.mockReset();
-    mockApi.mockReset();
-    mockApi.mockReturnValueOnce([mockCall, mockUnsubscribe]);
+    mockCall.mockResolvedValueOnce(responseData);
   });
 
-  it('render success', () => {});
+  it('render success', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useApi(apiResult));
+    await waitForNextUpdate();
+    expect(result.current).toBeTruthy();
+  });
+
+  it('hot api call should be success and states have right values', async () => {
+    const {
+      result: {
+        current: { name, age, call, error, loading, success, unsubscribe },
+      },
+      waitForNextUpdate,
+    } = renderHook(() => useApi(apiResult));
+
+    expect(name).toBeUndefined();
+    expect(age).toBeUndefined();
+
+    expect(call).toBeNull();
+    expect(error).toBeNull();
+    expect(loading).toBe(true);
+    expect(success).toBe(false);
+    expect(unsubscribe).toBeTruthy();
+
+    await waitForNextUpdate();
+
+    expect(name).toBe('dooboo');
+    expect(age).toBe(24);
+  });
 });
